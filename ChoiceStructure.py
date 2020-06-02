14384c18c854ac8e0a04d82a4b79d8db5ce088ac
Oskar Jensen <taraquad@gmail.com>
2020-06-02 02:24:46 -0700

    Finished ChoiceStructure Added constructor method from_cs() for deriving from another choice structure, and added comments for position and gray_edge. Still not entirely sure what the latter is but I described how it ultimately gets used.

--- ChoiceStructure.py
+++ ChoiceStructure.py
@@ -8,18 +8,29 @@ import PGMPath
 #                                                                 #
 # Based on ChoiceStructure.java from C.Zheng & D.Sankoff (2011)   #
 #                                                                 #
-# Author: Holger Jensen                                           #
+# Author: Holger Jensen, Oskar Jensen                             #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 class ChoiceStructure:
     def __init__(self):
         self.index_from = 0        # Object instance identifier
         self.for_which_genome = 0  # For which genome this structure refers to
         self.priority = 0          # Priority level for makeCycles algorithm (section 3.1.3)
-        self.position = 0          # TODO: What does this do?
+        self.position = 0          # Position of the choice structure in Priority's cs_index[]
         self.genome_1_path = None  # Path for genome 1
         self.genome_2_path = None  # Path for genome 2
         self.genome_3_path = None  # Path for genome 3
-        self.gray_edge = None      # TODO: What does this do?
+        self.gray_edge = None      # Gets used when updating paths & priorities in the makeCycles algorithm
+
+    @classmethod
+    def from_cs(cls, cs: ChoiceStructure):
+        cls.index_from = cs.index_from
+        cls.for_which_genome = cs.for_which_genome
+        cls.priority = cs.priority
+        cls.position = cs.position
+        cls.genome_1_path = cs.genome_1_path
+        cls.genome_2_path = cs.genome_2_path
+        cls.genome_3_path = cs.genome_3_path
+        cls.gray_edge = cs.gray_edge
 
     def __setattr__(self, key, value):
         self.key = value
