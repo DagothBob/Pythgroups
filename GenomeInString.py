38c6927d282772586e53cf239315088a5372d3a2
Holger <bo3b@live.com>
2020-06-09 19:26:44 -0700

    GenomeInString docs

--- GenomeInString.py
+++ GenomeInString.py
@@ -1,3 +1,21 @@
+"""
+ GenomeInString class for use in various classes
+
+ Based on GenomeInString.java from C.Zheng & D.Sankoff (2011)
+
+ Author: Holger Jensen
+"""
+
+
 class GenomeInString:
+    """
+    Attributes:
+        chromosomes: List of chromosomes expressed as strings
+    """
     def __init__(self, chromosomes: [str]):
+        """
+        Constructor
+
+        :param chromosomes: Chromosomes to construct from
+        """
         self.chromosomes: [str] = chromosomes
