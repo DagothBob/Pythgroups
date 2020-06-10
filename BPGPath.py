cadea0681949b9d24c3b392679c4b47c89a9961b
Holger <bo3b@live.com>
2020-06-09 18:49:08 -0700

    BPGPath docs

--- BPGPath.py
+++ BPGPath.py
@@ -1,24 +1,46 @@
 from __future__ import annotations
 
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# Path for use in a BreakPoint Graph for BPGDistance.   #
-#                                                       #
-# Based on BPGPath.java from C.Zheng & D.Sankoff (2011) #
-#                                                       #
-# Author: Holger Jensen                                 #
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+"""                            
+ Path for use in a BreakPoint Graph for BPGDistance.   
+                                                       
+ Based on BPGPath.java from C.Zheng & D.Sankoff (2011) 
+                                                       
+ Author: Holger Jensen                                 
+"""
+
+
 class BPGPath:
+    """
+    Attributes:
+        head: Head gene of the path
+        tail: Tail gene of the path
+        genome_head: Genome identifier for the head gene
+        genome_tail: Genome identifier for the tail gene
+    """
     def __init__(self, head: int or None, tail: int or None, ghead: int or None, gtail: int or None):
+        """
+        Constructor
+
+        :param head: Head gene
+        :param tail: Tail gene
+        :param ghead: Genome which gene head belongs to
+        :param gtail: Genome which gene tail belongs to
+        """
         if head is None or tail is None or ghead is None or gtail is None:
             return
         else:
-            self.head = head          # Head gene
-            self.tail = tail          # Tail gene
-            self.genome_head = ghead  # Genome for head
-            self.genome_tail = gtail  # Genome for tail
+            self.head = head
+            self.tail = tail
+            self.genome_head = ghead
+            self.genome_tail = gtail
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """
+        String override
+
+        :return: String representation of the BPGPath
+        """
         return "Head node is: " + str(self.head) + \
                "(" + str(self.genome_head) + ") | Tail node is: " + \
                str(self.tail) + "(" + str(self.genome_tail) + ")"
